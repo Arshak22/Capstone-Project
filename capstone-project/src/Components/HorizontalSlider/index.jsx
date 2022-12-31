e23b889beba382e0b9139948f880624040748abf
Arshak22 <arshak_kosakyan@edu.aua.am>
2022-12-31 16:24:12 +0400

    Start Working On a Slider

--- capstone-project/src/Components/HorizontalSlider/index.jsx
+++ capstone-project/src/Components/HorizontalSlider/index.jsx
@@ -3,15 +3,25 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./style.css";
+import { NavLink } from "react-router-dom";
 
-export default function HorizontalSlider() {
+//icons
+import { FaShareAlt } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
+
+export default function HorizontalSlider({movies}) {
     const settings = {
         dots: false,
         infinite: true,
-        speed: 500,
         slidesToShow: 4,
         slidesToScroll: 1,
         initialSlide: 0,
+        // autoplay: true,
+        speed: 5000,
+        autoplaySpeed: 2000,
+        cssEase: "linear",
+        pauseOnHover: true,
         responsive: [
           {
             breakpoint: 1024,
@@ -42,7 +52,37 @@ export default function HorizontalSlider() {
 
     return (
         <div className="sliderSection">
-            
+            <Slider {...settings}>
+                {movies.map((elem, index) => {
+                    return (
+                        <div className="movieBlock" key={index}>
+                            <div className="movieBlockImage">
+                                {elem.img ? <img src={elem.img} alt={elem.img} />: null}
+                            </div>
+                            <div className="movieBlockDescription">
+                                <h6><NavLink to="/movies" end>{elem.title}</NavLink></h6>
+                                <div>
+                                    <span>{elem.duration}</span>
+                                </div>
+                                <NavLink to="/movies" end><button>Read More</button></NavLink>
+                            </div>
+                            <div className="movieBlockSocialInfo">
+                                <ul>
+                                    <li className="shareIcon">
+                                        <span><FaShareAlt/></span>
+                                    </li>
+                                    <li className="favoriteIcon">
+                                        <span><FaHeart/></span>
+                                    </li>
+                                    <li className="watchListIcon">
+                                        <span><FaPlus/></span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    )
+                })}
+            </Slider>
         </div>
     )
 }
--- capstone-project/src/Pages/Home/index.jsx
+++ capstone-project/src/Pages/Home/index.jsx
@@ -4,12 +4,74 @@ import MainPicture from "../../Components/MainPicture";
 import SliderHeader from "../../Components/SliderHeader";
 import HorizontalSlider from "../../Components/HorizontalSlider";
 
+
+// Movie Pics
+import Movie1 from "../../assets/images/Movie1.jpg";
+import Movie2 from "../../assets/images/Movie2.jpg";
+import Movie3 from "../../assets/images/Movie3.jpg";
+import Movie4 from "../../assets/images/Movie4.jpg";
+import Movie5 from "../../assets/images/Movie5.jpg";
+import Movie6 from "../../assets/images/Movie6.jpg";
+import Movie7 from "../../assets/images/Movie7.jpg";
+import Movie8 from "../../assets/images/Movie8.jpg";
+
 export default function Home() {
+    const latestMovies = [
+        {
+            id: 1,
+            title: "Movie 1",
+            duration: "1hr 50m",
+            img: Movie1
+        },
+        {
+            id: 2,
+            title: "Movie 2",
+            duration: "2hr 50m",
+            img: Movie2
+        },
+        {
+            id: 3,
+            title: "Movie 3",
+            duration: "1hr 30m",
+            img: Movie3
+        },
+        {
+            id: 4,
+            title: "Movie 4",
+            duration: "1hr 20m",
+            img: Movie4
+        },
+        {
+            id: 5,
+            title: "Movie 5",
+            duration: "1hr 50m",
+            img: Movie5
+        },
+        {
+            id: 6,
+            title: "Movie 6",
+            duration: "2hr 50m",
+            img: Movie6
+        },
+        {
+            id: 7,
+            title: "Movie 7",
+            duration: "1hr 30m",
+            img: Movie7
+        },
+        {
+            id: 8,
+            title: "Movie 8",
+            duration: "1hr 20m",
+            img: Movie8
+        }
+    ];
+
     return (
         <div className="main">
             <MainPicture img={MainPic} headline="Welcome to the world of cinema"/>
             <SliderHeader header="Latest Movies" link="/movies" btnName="View All"/>
-            <HorizontalSlider/>
+            <HorizontalSlider movies={latestMovies}/>
         </div>
     );
 }
