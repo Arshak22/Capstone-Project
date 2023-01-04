1a15c4fd3d0fa4f474393689cb6e2f5e1a8e99f9
Arshak22 <arshak_kosakyan@edu.aua.am>
2023-01-04 17:02:56 +0400

    Link added to FanPage Section

--- capstone-project/src/Components/FanPageSection/index.jsx
+++ capstone-project/src/Components/FanPageSection/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 import Pic from "../../assets/images/Surprised-Deadpool.png";
+import { NavLink } from "react-router-dom";
 
 export default function FanPageSection() {
     return (
@@ -9,10 +10,10 @@ export default function FanPageSection() {
                 <div className="col">
                     <h1>Be Surprised by the Magic of Movies on Our Fan Page</h1>
                     <p>Join our movie community and be transported into the world of your favorite films! Our page is filled with stunning visual effects and exclusive movie-based content that will leave you wanting more. Keep up with the latest movie news, participate in discussions with other fans, and get access to behind-the-scenes content and special promotions. Click the button below to visit our Fan Page now and start exploring.</p>
-                    <button className="btn btn2">Explore More</button>
+                    <NavLink to="/fan-page" end><button className="btn btn2">Explore More</button></NavLink>
                 </div>
                 <div className="col">
-                    <img src={Pic} alt="FanPageSectionPic" id="FanPageSectionPic"/>
+                    <NavLink to="/fan-page" end><img src={Pic} alt="FanPageSectionPic" id="FanPageSectionPic"/></NavLink>
                 </div>
             </div>
         </div>
