4c8b5617d40f16945677bd3f798abe62029ff432
Arshak22 <arshak_kosakyan@edu.aua.am>
2023-01-06 02:03:55 +0400

    Team Section Done Responsive

--- capstone-project/src/Components/TeamMembers/index.jsx
+++ capstone-project/src/Components/TeamMembers/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./style.css";
 
+import { FaFacebookF } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import { FaTelegramPlane } from "react-icons/fa";
+
 export default function TeamMembers({team}) {
     return (
         <div className="Team">
@@ -11,6 +15,11 @@ export default function TeamMembers({team}) {
                         <div className="member">
                             <h3>{elem.name}</h3>
                             <h4>{elem.profession}</h4>
+                            <div className="memberSocial">
+                                <FaFacebookF className="memberSocialIcon"/>
+                                <RiInstagramFill className="memberSocialIcon"/>
+                                <FaTelegramPlane className="memberSocialIcon"/>
+                            </div>
                         </div>
                     </div>
                 )
