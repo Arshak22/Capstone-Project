0bb6d613fd3c6c8d907542ce115ae20ca3937267
Arshak22 <arshak_kosakyan@edu.aua.am>
2023-01-28 22:06:26 +0400

    MainImage css is changed

--- capstone-project/src/Components/MainPicture/index.jsx
+++ capstone-project/src/Components/MainPicture/index.jsx
@@ -3,8 +3,8 @@ import "./style.css";
 
 export default function MainPicture(props) {
     return (
-        <div className="mainImage">
-            {props.img ? <img src={props.img} alt="MainImage" style={{width: "100%", height: "100%"}}/>: null}
+        <div className="mainImage" style={{backgroundImage: `url(${props.img})`}}>
+            {/* {props.img ? <img src={props.img} alt="MainImage" style={{width: "100%", height: "100%"}}/>: null} */}
             <div className="centeredHeadline"><h1>{props.headline}</h1></div>
         </div>
     );
