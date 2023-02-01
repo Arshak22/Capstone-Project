69809ae89e8dc2d3311d0f0a442043795eee66d0
Arshak22 <arshak_kosakyan@edu.aua.am>
2023-02-01 16:04:02 +0400

    Favorites Controller Added

---
+++ capstone-project/src/assets/Platform/Favorites/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+import {API} from "../api";
+const favorites = "/favorites";
+
+export const getProfileFavorites = (profileID) => {
+    return axios.get(`${API}${favorites}/${profileID}`);
+}
+
+export const addFavorite = (profileID, movieID, movie) => {
+    return axios.post(`${API}${favorites}/${profileID}/${movieID}`, movie);
+}
+
+export const deleteFavorite = (profileID, movieID, movie) => {
+    return axios.delete(`${API}${favorites}/${profileID}/${movieID}`);
+}
