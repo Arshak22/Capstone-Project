e62967ca438d7b705b455784b6c1d9457e888fa4
Arshak22 <arshak_kosakyan@edu.aua.am>
2023-02-04 22:17:33 +0400

    Working on Cast Pop Up

--- capstone-project/src/Components/TopCast/index.jsx
+++ capstone-project/src/Components/TopCast/index.jsx
@@ -10,12 +10,24 @@ export default function TopCast({movie}) {
                     const date = new Date(elem.birthDate);
                     const dateString = date.toLocaleDateString('en-US', {day: 'numeric', month: 'short', year: 'numeric'});
                     return (
-                        <div className="castMember" key={index} style={{backgroundImage: `url(https://www.themoviedb.org/t/p/original/${elem.imagePath}.jpg)`}}>
-                            <div className="castMemberInfo">
-                                <h4>{elem.firstName} {elem.lastName}</h4>
-                                <h6>{dateString}</h6>
+                        <>
+                            {/* <div className="castMember" key={index} style={{backgroundImage: `url(https://www.themoviedb.org/t/p/original/${elem.imagePath}.jpg)`}}>
+                                <div className="castMemberInfo">
+                                    <h4>{elem.firstName} {elem.lastName}</h4>
+                                    <h6>{dateString}</h6>
+                                </div>
+                            </div> */}
+                            <div className="castPopUp">
+                                <div className="castPopUpLCol" style={{backgroundImage: `url(https://www.themoviedb.org/t/p/original/${elem.imagePath}.jpg)`}}>
+                                </div>
+                                <div className="castPopUpRCol">
+                                    <h2>{elem.firstName} {elem.lastName}</h2>
+                                    <h4>{dateString}</h4>
+                                    <p>{elem.bio}</p>
+                                </div>
                             </div>
-                        </div>
+                        </>
+                        
                     )
                 })}
             </div>
--- capstone-project/src/Pages/DefaultMoviePage/index.jsx
+++ capstone-project/src/Pages/DefaultMoviePage/index.jsx
@@ -3,29 +3,10 @@ import { useParams } from 'react-router-dom';
 import { getWatchable } from "../../Platform/Watchables";
 import { getActors } from "../../Platform/Actors";
 
-import Poster from "../../assets/images/DefaultMoviePoster.jpg";
-import Backdrop from "../../assets/images/DefaultMovieBackdrop.jpg";
 import MovieInfoSection from "../../Components/MovieInfoSection";
 import TopCast from "../../Components/TopCast";
 import MovieBackdropSlider from "../../Components/MovieBackdropSlider";
 import MovieTrailerSection from "../../Components/MovieTrailerSection";
-
-//Cast Pictures
-import Cast1 from "../../assets/images/TopCast/Cast1.jpg";
-import Cast2 from "../../assets/images/TopCast/Cast2.jpg";
-import Cast3 from "../../assets/images/TopCast/Cast3.jpg";
-import Cast4 from "../../assets/images/TopCast/Cast4.jpg";
-import Cast5 from "../../assets/images/TopCast/Cast5.jpg";
-import Cast6 from "../../assets/images/TopCast/Cast6.jpg";
-import Cast7 from "../../assets/images/TopCast/Cast7.jpg";
-
-//Backdrop Image Examples
-import BD1 from "../../assets/images/BackDrops/BD1.jpg";
-import BD2 from "../../assets/images/BackDrops/BD2.jpg";
-import BD3 from "../../assets/images/BackDrops/BD3.jpg";
-import BD4 from "../../assets/images/BackDrops/BD4.jpg";
-import BD5 from "../../assets/images/BackDrops/BD5.jpg";
-import BD6 from "../../assets/images/BackDrops/BD6.jpg";
 
 export function DefaultMoviePage() {
     const {id} = useParams();
@@ -34,7 +15,7 @@ export function DefaultMoviePage() {
     useEffect(() => {
         getMovie(id);
         getAllActors();
-    }, [])
+    }, [id])
 
     useEffect(() => {
         if(movieData) {
@@ -60,63 +41,12 @@ export function DefaultMoviePage() {
         }
     }
 
-    const movieInfo = {
-        title: "Deadpool",
-        releaseDate: "02/12/2016",
-        genres: ["Action" , "Adventure", "Comedy"],
-        duration: "1h 48m",
-        rating: 76,
-        overview: "The origin story of former Special Forces operative turned mercenary Wade Wilson, who, after being subjected to a rogue experiment that leaves him with accelerated healing powers, adopts the alter ego Deadpool. Armed with his new abilities and a dark, twisted sense of humor, Deadpool hunts down the man who nearly destroyed his life.",
-        poster: Poster,
-        backdrop: Backdrop,
-        directors: ["Tim Miller"],
-        cast: [
-            {
-                name: "Ryan Reynolds",
-                role: "Wade Wilson / Deadpool",
-                image: Cast1
-            },
-            {
-                name: "Morena Baccarin",
-                role: "Vanessa",
-                image: Cast2
-            },
-            {
-                name: "Ed Skrein",
-                role: "Ajax",
-                image: Cast3
-            },
-            {
-                name: "T.J. Miller",
-                role: "Weasel",
-                image: Cast4
-            },
-            {
-                name: "Gina Carano",
-                role: "Angel Dust",
-                image: Cast5
-            },
-            {
-                name: "Karan Soni",
-                role: "Dopinder",
-                image: Cast6
-            },
-            {
-                name: "Brianna Hildebrand",
-                role: "Negasonic Teenage Warhead",
-                image: Cast7
-            }
-        ],
-        backdrops: [BD1, BD2, BD3, BD4, BD5, BD6],
-        trailerID: "ONHBaC-pfsk"
-    }
-
     return (
         <div className="main">
             {movieData ? 
                 <>
                     <MovieInfoSection movie={movieData}/>
-                    <TopCast movie={allActors}/>
+                    {allActors ? <TopCast movie={allActors}/>: null}
                     <MovieBackdropSlider movie={movieData}/>
                     <MovieTrailerSection movie={movieData}/>
                 </>
