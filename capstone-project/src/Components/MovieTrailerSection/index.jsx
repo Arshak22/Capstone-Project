3d62d15aa5423449a1e49c5b740c4ea35a010ea3
Arshak22 <arshak_kosakyan@edu.aua.am>
2023-02-04 18:53:59 +0400

    No Trailer Section Added

--- capstone-project/src/Components/MovieTrailerSection/index.jsx
+++ capstone-project/src/Components/MovieTrailerSection/index.jsx
@@ -26,7 +26,10 @@ export default function MovieTrailerSection({movie}) {
     <div className={`split-section ${isActive ? 'active' : ''}`}>
       <div className="split-section__inner">
         <div className={`split-section__content ${isActive ? 'active' : ''}`}>
-            {movie.trailerLink ? <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${movie.trailerLink}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen className='trailer'></iframe>: null}
+            {movie.trailerLink ? <iframe title='movieTrailer' width="100%" height="100%" src={`https://www.youtube.com/embed/${movie.trailerLink}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen className='trailer'></iframe>: 
+            <div className='noTrailerYet'>
+                <h2>Sorry, no trailer yet</h2>
+            </div>}
         </div>
       </div>
     </div>
