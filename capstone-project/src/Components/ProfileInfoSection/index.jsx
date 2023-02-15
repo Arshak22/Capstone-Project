c3e7013b0299dc1ab7640af8e82556e7b022ad5e
Arshak22 <arshak_kosakyan@edu.aua.am>
2023-02-15 19:49:02 +0400

    Working on Edit Profile Component

--- capstone-project/src/Components/ProfileInfoSection/index.jsx
+++ capstone-project/src/Components/ProfileInfoSection/index.jsx
@@ -29,6 +29,16 @@ export default function ProfileInfoSection() {
                         <h2 className={active === "Comments" ? "activeSmallBar" : ""} onClick={() => handleClick("Comments")}>My Comments</h2>
                         <h2 className={active === "Edit" ? "activeSmallBar" : ""} onClick={() => handleClick("Edit")}>Edit Profile</h2>
                     </div>
+                    {active === "Edit" ? 
+                    <div className="editProfile">
+                        <input className="editInput" type="text" name="firstName" placeholder="First Name" required/>
+                        <input className="editInput" type="text" name="lastName" placeholder="Last Name" required/>
+                        <input className="editInput" type="email" name="email" placeholder="Email Address" required/>
+                        <input className="editInput" type="password" name="password" placeholder="Password" required/>
+                        <input className="editInput" type="password" name="newPassword" placeholder="New Password" required/>
+                        <button className="btn btn2">Edit Profile</button>
+                        <button className="btn btn2">Delete Profile</button>
+                    </div>: null}
                 </div>
             </div>
         </div>
