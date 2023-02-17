49d352164d20daef03c63ed094e7553322b20a59
Arshak22 <arshak_kosakyan@edu.aua.am>
2023-02-17 17:08:38 +0400

    Some Fixes

--- capstone-project/src/Pages/Profile/index.jsx
+++ capstone-project/src/Pages/Profile/index.jsx
@@ -34,10 +34,10 @@ export default function Profile() {
         <div className="profileContainer">
             <div className="sideBar">
                 <div className="rowspan">
-                <h3 className={active === "Profile" ? "activeSideBar" : ""} onClick={() => handleClick("Profile")}>Profile</h3>
-                <h3 className={active === "Favourites" ? "activeSideBar" : ""} onClick={() => handleClick("Favourites")}>Favourites</h3>
-                <h3 className={active === "Watchlist" ? "activeSideBar" : ""} onClick={() => handleClick("Watchlist")}>Watchlist</h3>
-                <h3>Log Out</h3>
+                    <h3 className={active === "Profile" ? "activeSideBar" : ""} onClick={() => handleClick("Profile")}>Profile</h3>
+                    <h3 className={active === "Favourites" ? "activeSideBar" : ""} onClick={() => handleClick("Favourites")}>Favourites</h3>
+                    <h3 className={active === "Watchlist" ? "activeSideBar" : ""} onClick={() => handleClick("Watchlist")}>Watchlist</h3>
+                    <h3>Log Out</h3>
                 </div> 
             </div>
             <div className="profileRCol">
